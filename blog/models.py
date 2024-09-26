1d38556c1f1e0fe0ad6e2d79f91ae9fa3d52134c
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-26 15:16:59 +0000

    metadata to your models

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,15 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
 
+    #def __str__(self):
+    #    return f"The title of this post is {self.title}"
+
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -26,5 +34,6 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
-
+    class Meta:
+        ordering = ["-created_on"]
 
